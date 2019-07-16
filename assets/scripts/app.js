5234dd3de661c83d86e22488834425f78a78587c
Brandt Strom <brandtkstrom@gmail.com>
2019-07-15 23:17:50 -0700

    Set current time interval when DOM is loaded.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,13 +1,6 @@
-
-
-
-
-
-
-function displayRealTime() {
-    setInterval(function () {
-        $('#current-time').html(moment().format('hh:mm A'))
+document.addEventListener("DOMContentLoaded", evt => {
+    // Create interval to update displayed current time
+    setInterval(function() {
+        $("#current-time").html(moment().format("hh:mm:ss A"));
     }, 1000);
-}
-displayRealTime();
-
+});
