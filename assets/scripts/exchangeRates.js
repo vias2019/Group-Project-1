4bc1be152601773b519fb2cc1936cfc116e3d708
Brandt Strom <brandtkstrom@gmail.com>
2019-07-16 20:21:15 -0700

    add currency conversions

--- assets/scripts/exchangeRates.js
+++ assets/scripts/exchangeRates.js
@@ -1,18 +1,16 @@
-const API_URL = "https://api.exchangeratesapi.io/latest?base=USD";
+const RATES_API = 'https://api.exchangeratesapi.io/latest?base=USD';
+
+async function getCurrencyConversions() {
+    let response = await fetch(RATES_API);
+    let data = await response.json();
+
+    window.rates = data.rates;
+
+    return window.rates;
+}
 
 // Converts the specified currency and value to USD
-async function convertToUSD(value, currency) {
-    // Lazily init a global rates object
-    if (!window.rates) {
-        let response = await fetch(API_URL);
-        let data = await response.json();
-
-        console.log("Currency conversion table fetched: ");
-        console.log(data);
-
-        window.rates = data.rates;
-    }
-
+function convertToUSD(value, currency) {
     // Index into currency conversion table for specified currency
     if (window.rates[currency]) {
         return parseFloat(value / window.rates[currency]).toFixed(2);
@@ -22,7 +20,9 @@ async function convertToUSD(value, currency) {
     return value;
 }
 
-// test api call
-// todo - remove
-console.log("Convert 65.24 CAD to USD:");
-convertToUSD(65.24, "CAD").then(val => console.log(val));
+document.addEventListener('DOMContentLoaded', () => {
+    getCurrencyConversions().then(data => {
+        console.log('Currency conversion table fetched: ');
+        console.log(data);
+    });
+});
